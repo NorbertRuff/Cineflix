f01d1ce55ee69843688c03a0a536a151950b7911
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-15 18:49:01 +0200

    [add] search components styled properties

---
+++ src/styles/SearchPage.Styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 4rem;
+`;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: var(--fs-base);
+`;
+
+
+export const TitleContainer = styled.div`
+  text-align: center;
+`;
+
