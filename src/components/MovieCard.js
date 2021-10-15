3261871a6025680dba54eb914af271ec2733636b
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-15 18:50:45 +0200

    [add] js date converter function

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {GenresContainer, SingleElementContainer} from "../styles/MovieCard.Styled";
 
-
+function convertIsoDate(releaseDate) {
+    let date = new Date(releaseDate)
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+}
 const MovieCard = (props) => {
     return (
         <SingleElementContainer key={props.movie.id}>
             <h2>{props.movie.name}</h2>
             <img src={props.movie.img.url} alt={props.movie.name}/>
-
+            <h3>{convertIsoDate(props.movie.releaseDate)}</h3>
             <GenresContainer>
                 {props.movie.genres.length !== 0 ? props.movie.genres.map(genre =>
                     <div key={genre.name}>
