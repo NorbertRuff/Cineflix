ad4075eafda1696f9e02b2fc8f67993c2b3e901d
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-15 21:31:19 +0200

    [add] apollo get movie query

---
+++ src/graphQL/Queries.js
@@ -0,0 +1,19 @@
+import {gql} from "@apollo/client";
+
+export const GET_MOVIES = gql`
+                query SearchMovies($keyWord: String!) {
+                searchMovies(query: $keyWord) {
+                    id
+                    name
+                    overview
+                    releaseDate
+                    img: poster {
+                        url: custom(size: "w185_and_h278_bestv2")
+                    }
+                    genres {
+                    ... on Genre{
+                            name
+                        }
+                    }
+                }
+            }`;
