7b464e7c9427587ee387affb12ed226f156b87c4
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-15 21:32:47 +0200

    [add] ApolloProvider and client

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import {Footer, Header, PageContainerStyle} from "./styles/PageContainer.Style";
 import MainContainer from "./components/MainContainer";
+import {ApolloClient, ApolloProvider, InMemoryCache,} from "@apollo/client";
+
+
+const client = new ApolloClient({
+    uri: 'https://tmdb.sandbox.zoosh.ie/dev/graphql',
+    cache: new InMemoryCache()
+});
 
 function App() {
     return (
-        <PageContainerStyle>
-            <Header>
-                <h2>Navbar</h2>
-            </Header>
-            <MainContainer/>
-            <Footer>
-                <h4>Created by Ruff Norbert</h4>
-            </Footer>
-        </PageContainerStyle>
+        <ApolloProvider client={client}>
+            <PageContainerStyle>
+                <Header>
+                    <h2>Navbar</h2>
+                </Header>
+                <MainContainer/>
+                <Footer>
+                    <h4>Created by Ruff Norbert</h4>
+                </Footer>
+            </PageContainerStyle>
+        </ApolloProvider>
     );
 }
 
