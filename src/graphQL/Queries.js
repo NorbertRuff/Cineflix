dcaaf62616f27e8b4a0cba0f0a6d0ba6107e0c0c
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-16 17:04:37 +0200

    [add] query for movie details

--- src/graphQL/Queries.js
+++ src/graphQL/Queries.js
@@ -17,4 +17,44 @@ export const GET_MOVIES = gql`
                         }
                     }
                 }
-            }`;
+            }`
+
+export const GET_MOVIE_DETAILS = gql`
+query getMovie($ID: ID!) {
+  movie(id: $ID) {
+    id
+    name
+    overview
+    score
+    releaseDate
+    img: poster {
+      url: custom(size: "w185_and_h278_bestv2")
+    }
+    backdrop {small}
+    backdrop {large}
+    cast(limit: 5) {
+      id
+      person {
+        name
+        photo {small}
+      }
+      role {
+        ... on Cast {
+          character
+        }
+      }
+    }
+    crew(limit: 5) {
+      id
+      person {
+        name
+      }
+      role {
+        ... on Crew {
+          job
+          department
+        }
+      }
+    }
+  }
+}`;
