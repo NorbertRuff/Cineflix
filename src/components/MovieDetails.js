bfe83bf85adc27b33fd4446a73b7180976a1851b
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-17 13:59:59 +0200

    [update] data handling moved to mini card components

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {ErrorMessage, LoadingMessage, MainContentWrapper} from "../styles/PageContainer.Style";
 import {useQuery} from "@apollo/client";
 import {GET_MOVIE_DETAILS} from "../graphQL/Queries";
@@ -8,40 +8,20 @@ import MovieCast from "./CardComponents/MovieCast";
 import MovieCrew from "./CardComponents/MovieCrew";
 import MovieOverView from "./CardComponents/MovieOverView";
 import MovieThumbnail from "./CardComponents/MovieThumbnail";
-import {dataHandler} from "../services/dataHandler";
 import {MovieDetailsWrapper} from "../styles/MovieDetails.Styled";
 import WikiInfoCard from "./WikiInfoCard";
 import ImdbInfoCard from "./ImdbdInfoCard";
 
+
 const MovieDetails = (props) => {
     const movieId = props.match.params.id;
-
-    function fetchWikiInfo(name) {
-        const baseUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srlimit=1&srsearch=${name}&srnamespace=0&srprop=snippet`;
-        dataHandler._api_get(baseUrl, setWikiInfo, setWikiError, setWikiLoading)
-    }
-
-    function fetchImdbInfo(name) {
-        const baseUrl = `https://imdb-api.com/en/API/SearchMovie/k_13fjgtth/${name.toString()}`;
-        dataHandler._api_get(baseUrl, setImdbInfo, setImdbError, setImdbLoading)
-    }
 
     const {data, loading, error} = useQuery(GET_MOVIE_DETAILS, {
         variables: {ID: movieId},
         onCompleted: data => {
-            console.log('data ', data);
-            fetchWikiInfo(data.movie.name)
-            fetchImdbInfo(data.movie.name)
+            // console.log('data ', data);
         }
     });
-
-    const [wikiInfo, setWikiInfo] = useState("");
-    const [wikiLoading, setWikiLoading] = useState(false);
-    const [wikiError, setWikiError] = useState(false);
-
-    const [imdbInfo, setImdbInfo] = useState("");
-    const [imdbLoading, setImdbLoading] = useState(false);
-    const [imdbError, setImdbError] = useState(false);
 
     if (loading) {
         return (
@@ -82,10 +62,10 @@ const MovieDetails = (props) => {
                 </Card>
                 <Box sx={{maxWidth: 400, display: "flex", flexDirection: "column"}}>
                     <Card className="wikiCard" sx={{marginBottom: 2}}>
-                        <WikiInfoCard wikiInfo={wikiInfo} wikiLoading={wikiLoading} wikiError={wikiError}/>
+                        <WikiInfoCard MovieInfo={data.movie.name}/>
                     </Card>
                     <Card className="imdbCard" sx={{marginBottom: 2}}>
-                        <ImdbInfoCard imdbInfo={imdbInfo} imdbLoading={imdbLoading} imdbError={imdbError}/>
+                        <ImdbInfoCard MovieInfo={data.movie.name}/>
                     </Card>
                 </Box>
             </MovieDetailsWrapper>
