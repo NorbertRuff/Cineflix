21b7fe0ac1301399b61ef0b558e6b7fbb1b8f123
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-17 14:05:11 +0200

    [add] card media query

--- src/styles/MovieDetails.Styled.js
+++ src/styles/MovieDetails.Styled.js
@@ -4,4 +4,9 @@ export const MovieDetailsWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: var(--fs-base);
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
