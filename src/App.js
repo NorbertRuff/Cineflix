3e1aa21d2846f79e390709ea0a04dd3b791733bf
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-17 20:50:02 +0200

    [add] route to related movies

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MovieDetails from "./components/MovieDetails";
 import {ApolloClient, ApolloProvider, InMemoryCache,} from "@apollo/client";
 import GithubCorner from "react-github-corner";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import RelatedMovies from "./components/RelatedMovies";
 
 
 const client = new ApolloClient({
@@ -27,6 +28,8 @@ function App() {
                         </Route>
                         <Route path="/movie/:id"
                                render={(props) => <MovieDetails {...props}/>}/>
+                        <Route path="/related/:id"
+                               render={(props) => <RelatedMovies {...props}/>}/>
                     </Switch>
                     <Footer>
                         <h4>Created by Ruff Norbert</h4>
