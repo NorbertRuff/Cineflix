d79be2aee7cb10f8adc5dd84679c52b891c16396
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-18 08:20:10 +0200

    [add] related button

--- src/components/CardComponents/MovieResultComponent.js
+++ src/components/CardComponents/MovieResultComponent.js
@@ -2,16 +2,24 @@ import React from 'react';
 import {ResultContainer} from "../../styles/SearchPage.Styled";
 import {Link} from "react-router-dom";
 import MovieCard from "../MovieCard";
+import {Box, Button} from "@mui/material";
 
-const MovieResultCardComponent = ({movies}) => {
+const MovieResultCardComponent = ({movies, handleSearchRelatedMovies, setRelatedMovie}) => {
     return (
         <ResultContainer>
             {movies && (
                 movies.length === 0 ? <h1>No result</h1> :
                     movies.map(movie =>
-                        <Link key={movie.id} to={`/movie/${movie.id}`}>
-                            <MovieCard movie={movie}/>
-                        </Link>
+                        <Box key={movie.id} sx={{display: "flex", flexDirection: "column"}}>
+                            <Link to={`/movie/${movie.id}`}>
+                                <MovieCard movie={movie}/>
+                            </Link>
+                            <Button onClick={() => {
+                                setRelatedMovie(movie);
+                                handleSearchRelatedMovies(movie.id)
+                            }}>Find related Movies
+                            </Button>
+                        </Box>
                     ))}
         </ResultContainer>
     );
